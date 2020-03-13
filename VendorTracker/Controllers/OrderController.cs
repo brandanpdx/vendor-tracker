70478f0ba591c2b0768b618d63d7d489e6300ad2
Brandan Sayarath <brandan@brandan.tech>
2020-03-13 13:31:31 -0700

    add OrderController.cs

---
+++ VendorTracker/Controllers/OrderController.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+using System;
+using Microsoft.AspNetCore.Mvc;
+using VendorTracker.Models;
+
+namespace VendorTracker.Controllers
+{
+  public class AlbumController : Controller
+  {
+    [HttpGet("/vendor/{vendorId}/order/{orderId}")]
+    public ActionResult Show(int vendorId, int orderId)
+    {
+      Order order = Order.Find(orderId);
+      Vendor vendor = Vendor.Find(vendorId);
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      model.Add("order", order);
+      model.Add("vendor", vendor);
+      return View(model);
+    }
+  }
+  
+}
