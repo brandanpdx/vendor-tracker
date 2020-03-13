a8b02c609d702211caf3180206a97cc30f9c4e08
Brandan Sayarath <brandan@brandan.tech>
2020-03-13 10:18:32 -0700

    add vendor CSHTML and Vendor Controller

---
+++ VendorTracker/Controllers/VendorController.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+using System;
+using Microsoft.AspNetCore.Mvc;
+using VendorTracker.Models;
+
+namespace VendorTracker.Controllers
+{
+  public class VendorController : Controller
+  {
+    [HttpGet("/vendor")]
+    public ActionResult Index()
+    {
+
+    }
+  }
+}
